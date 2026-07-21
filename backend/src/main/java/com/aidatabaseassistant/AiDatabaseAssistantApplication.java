6831dcbd898a519e897dffb26768fbaa3b5880cf
Muhammad Taimoor Ajmal <taimoorajmal00@gmail.com>
2026-07-21 10:13:21 +0500

    Initial Spring Boot setup

---
+++ backend/src/main/java/com/aidatabaseassistant/AiDatabaseAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.aidatabaseassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDatabaseAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDatabaseAssistantApplication.class, args);
+    }
+
+}
