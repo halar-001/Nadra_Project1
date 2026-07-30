e92f485de040f1642eaeeace182e23dba4392783
Muhammad Taimoor Ajmal <taimoorajmal00@gmail.com>
2026-07-30 09:44:19 +0500

    Fix: Remove Frontend and database folders from backend branch

--- Frontend/src/pages/Register.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Login from "./Login";
-
-// Register routes simply point to Login but initialize with the "register" tab active
-export const Register = () => {
-  return <Login initialTab="register" />;
-};
-
-export default Register;
